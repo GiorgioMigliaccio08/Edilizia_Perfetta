9ecdda828f438ea35d5560648b9002e37acf2737
Giorgio Migliaccio <giorgiomigliaccio08@gmail.com>
2025-02-11 19:42:57 +0100

    Add Js and modify Css

---
+++ index.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const selectedLanguage = document.getElementById("selected-language");
+  const languageOptions = document.querySelectorAll(".dropdown li");
+
+  languageOptions.forEach((option) => {
+    option.addEventListener("click", function () {
+      selectedLanguage.textContent = this.textContent.split(" ")[1] + " ▼";
+    });
+  });
+});
